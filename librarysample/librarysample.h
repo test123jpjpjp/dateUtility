0847b7541441f9baa02b1a5cd3a90d6a47578c10
Ebina, Daisuke <ts-daisuke.ebina@rakuten.com>
2020-03-19 17:59:33 +0900

    Initial Commit

---
+++ librarysample/librarysample.h
@@ -0,0 +1,19 @@
+//
+//  librarysample.h
+//  librarysample
+//
+//  Created by Ebina, Daisuke on 2020/03/19.
+//  Copyright © 2020 Ebina, Daisuke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for librarysample.
+FOUNDATION_EXPORT double librarysampleVersionNumber;
+
+//! Project version string for librarysample.
+FOUNDATION_EXPORT const unsigned char librarysampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <librarysample/PublicHeader.h>
+
+
