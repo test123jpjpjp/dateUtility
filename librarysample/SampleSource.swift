cf5941e84e5e0eb62ba822552e7fa9831b1bd6fb
Ebina, Daisuke <ts-daisuke.ebina@rakuten.com>
2020-03-20 14:00:39 +0900

    first commit

---
+++ librarysample/SampleSource.swift
@@ -0,0 +1,13 @@
+//
+//  SampleSource.swift
+//  librarysample
+//
+//  Created by Ebina, Daisuke on 2020/03/19.
+//  Copyright © 2020 Ebina, Daisuke. All rights reserved.
+//
+
+import UIKit
+
+class SampleSource: NSObject {
+
+}
