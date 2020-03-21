356ccb1321a111dbb61c4bc64ce4e2a5b7caf159
Ebina, Daisuke <ts-daisuke.ebina@rakuten.com>
2020-03-21 16:24:50 +0900

    update : versionup

--- librarysample/SampleSource.swift
+++ librarysample/SampleSource.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 public extension Date {
-    func firstOfMonth() -> Date {
+    var firstOfMonth: Date {
         let startDate = Calendar.current.startOfDay(for: self)
         return Calendar.current.date(
             from: Calendar.current.dateComponents([.year, .month], from: startDate)) ?? Date()
     }
     
-    func lastOfMonth() -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: DateComponents(month: 1, day: -1), to: self.firstOfMonth()) ?? Date()
+    var lastOfMonth: Date {
+        return Calendar(identifier: .gregorian).date(byAdding: DateComponents(month: 1, day: -1), to: self.firstOfMonth) ?? Date()
 
     }
 }
